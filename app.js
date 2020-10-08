eb5914fd40d55ea147a40c2fe0e2904dad8ce9c7
Vi Tran <vitran86@icloud.com>
2020-10-07 21:36:58 -0400

    build dynamic navbar by JS, add DOMContentLoad,check section in viewport & add active class

--- app.js
+++ app.js
@@ -1,95 +1,6 @@
 console.log(`this file was created by Vi`);
 
-// Handle burger menu click//
-
-const menuBtn = document.querySelector("button.burger-menu");
-menuBtn.addEventListener("click", function (e) {
-  if (menuBtn.classList.contains("clicked")) {
-    menuBtn.classList.remove("clicked");
-  } else {
-    menuBtn.classList.add("clicked");
-  }
-});
-
-// Slider image in welcome section //
-
-const sliderImages = Array.from(document.querySelectorAll(".main-pic"));
-
-setInterval(function () {
-  const findIndex = sliderImages.findIndex(function (sliderImage) {
-    return sliderImage.classList.contains("show");
-  });
-  let nextIndex = (findIndex + 1) % sliderImages.length; // 1,2,0
-  sliderImages[findIndex].classList.remove("show");
-  sliderImages[nextIndex].classList.add("show");
-}, 2000);
-
-// Collapse button - service section//
-
-const collapeBtn = document.querySelector("button.collapsible");
-const serviceCards = Array.from(document.querySelectorAll(".service-item"));
-const filteredHiddenCards = serviceCards.filter(function (serviceCard) {
-  return serviceCard.classList.contains("hidden");
-});
-
-filteredHiddenCards.forEach(function (hiddenCard) {
-  collapeBtn.addEventListener("click", function (e) {
-    if (hiddenCard.classList.contains("hidden")) {
-      hiddenCard.classList.remove("hidden");
-      collapeBtn.classList.add("clicked");
-      e.target.textContent = "Collapse";
-    } else {
-      hiddenCard.classList.add("hidden");
-      collapeBtn.classList.remove("clicked");
-      e.target.textContent = "See more";
-    }
-  });
-});
-// Button on technician cards//
-
-const cardProfiles = document.querySelectorAll(".tag");
-
-cardProfiles.forEach(function (cardProfile) {
-  const introduceBtn = cardProfile.querySelector("button.introduce");
-  introduceBtn.addEventListener("click", function (e) {
-    if (
-      cardProfile.classList.contains("active") &&
-      introduceBtn.classList.contains("clicked")
-    ) {
-      e.target.textContent = "Introduce";
-      cardProfile.classList.remove("active");
-      introduceBtn.classList.remove("clicked");
-    } else {
-      e.target.textContent = "Close";
-      cardProfile.classList.add("active");
-      introduceBtn.classList.add("clicked");
-    }
-  });
-});
-
-//Button for appointment request//
-
-const aptmBtn = document.querySelector(".post-btn");
-
-aptmBtn.addEventListener("click", function (e) {
-  if (aptmBtn.classList.contains("clicked")) {
-    e.target.textContent = "Request";
-    aptmBtn.classList.remove("clicked");
-  } else {
-    e.target.textContent = "Requested";
-    aptmBtn.classList.add("clicked");
-  }
-});
-
-// Button Back-to-top//
-
-const backToTopBtn = document.querySelector(".back-to-top");
-
-backToTopBtn.addEventListener("click", function () {
-  window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
-});
-
-// Scroll to another sections //
+// Define variable
 const sections = [
   {
     menuId: "section-welcome",
@@ -113,6 +24,33 @@ const sections = [
   },
 ];
 
+// Building navbar as anorder list
+function initialSetup() {
+  const navBar = document.querySelector("ul.nav__links");
+  const fragment = document.createDocumentFragment();
+  sections.forEach((section) => {
+    const newNavButton = makeNewNavButtion(section);
+    newNavButton.addEventListener("click", function () {
+      scrollToSection(section.sectionId);
+    });
+    fragment.appendChild(newNavButton);
+  });
+  navBar.appendChild(fragment);
+}
+
+function makeNewNavButtion(section) {
+  const newNavButton = document.createElement("li");
+  newNavButton.setAttribute("id", section.menuId);
+  if (section.sectionId === "welcome") {
+    newNavButton.textContent = "Home";
+  } else {
+    newNavButton.textContent = section.sectionId;
+  }
+  return newNavButton;
+}
+
+// Scroll to another sections //
+
 function scrollToSection(id) {
   const element = document.getElementById(id);
   console.log(element.offsetTop);
@@ -123,9 +61,146 @@ function scrollToSection(id) {
   });
 }
 
-sections.forEach((section) => {
-  const menuLink = document.getElementById(section.menuId);
-  return menuLink.addEventListener("click", function () {
-    scrollToSection(section.sectionId);
+// Handle burger menu click//
+
+function setUpBurgerBtn() {
+  const menuBtn = document.querySelector("button.burger-menu");
+  menuBtn.addEventListener("click", function (e) {
+    if (menuBtn.classList.contains("clicked")) {
+      menuBtn.classList.remove("clicked");
+    } else {
+      menuBtn.classList.add("clicked");
+    }
   });
+}
+
+// Slider image in welcome section //
+
+function setUpSliderImages() {
+  const sliderImages = Array.from(document.querySelectorAll(".main-pic"));
+
+  setInterval(function () {
+    const findIndex = sliderImages.findIndex(function (sliderImage) {
+      return sliderImage.classList.contains("show");
+    });
+    let nextIndex = (findIndex + 1) % sliderImages.length; // 1,2,0
+    sliderImages[findIndex].classList.remove("show");
+    sliderImages[nextIndex].classList.add("show");
+  }, 2000);
+}
+
+// Collapse button - service section//
+
+function setUpCollapseBtn() {
+  const collapeBtn = document.querySelector("button.collapsible");
+  const serviceCards = Array.from(document.querySelectorAll(".service-item"));
+  const filteredHiddenCards = serviceCards.filter(function (serviceCard) {
+    return serviceCard.classList.contains("hidden");
+  });
+
+  filteredHiddenCards.forEach(function (hiddenCard) {
+    collapeBtn.addEventListener("click", function (e) {
+      if (hiddenCard.classList.contains("hidden")) {
+        hiddenCard.classList.remove("hidden");
+        collapeBtn.classList.add("clicked");
+        e.target.textContent = "Collapse";
+      } else {
+        hiddenCard.classList.add("hidden");
+        collapeBtn.classList.remove("clicked");
+        e.target.textContent = "See more";
+      }
+    });
+  });
+}
+// Button on technician cards//
+
+function setUpCardProfileTech() {
+  const cardProfiles = document.querySelectorAll(".tag");
+
+  cardProfiles.forEach(function (cardProfile) {
+    const introduceBtn = cardProfile.querySelector("button.introduce");
+    introduceBtn.addEventListener("click", function (e) {
+      if (
+        cardProfile.classList.contains("active") &&
+        introduceBtn.classList.contains("clicked")
+      ) {
+        e.target.textContent = "Introduce";
+        cardProfile.classList.remove("active");
+        introduceBtn.classList.remove("clicked");
+      } else {
+        e.target.textContent = "Close";
+        cardProfile.classList.add("active");
+        introduceBtn.classList.add("clicked");
+      }
+    });
+  });
+}
+
+//Button for appointment request//
+
+function setUpAppointmentBtn() {
+  const aptmBtn = document.querySelector(".post-btn");
+
+  aptmBtn.addEventListener("click", function (e) {
+    if (aptmBtn.classList.contains("clicked")) {
+      e.target.textContent = "Request";
+      aptmBtn.classList.remove("clicked");
+    } else {
+      e.target.textContent = "Requested";
+      aptmBtn.classList.add("clicked");
+    }
+  });
+}
+
+// Button Back-to-top//
+
+function setupBackToTopBtn() {
+  const backToTopBtn = document.querySelector(".back-to-top");
+
+  backToTopBtn.addEventListener("click", function () {
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+  });
+}
+
+// Call functions when DOM is ready
+document.addEventListener("DOMContentLoaded", function () {
+  initialSetup();
+  setUpBurgerBtn();
+  setUpSliderImages();
+  setUpCollapseBtn();
+  setUpCardProfileTech();
+  setUpAppointmentBtn();
+  setupBackToTopBtn();
 });
+
+// Check which section is in viewport, then add active class for it
+
+document.addEventListener(
+  "scroll",
+  function () {
+    checkSectionInViewport();
+  },
+  {
+    passive: true,
+  }
+);
+
+function checkSectionInViewport() {
+  sections.forEach((section) => {
+    const sectionElement = document.getElementById(`${section.sectionId}`);
+    if (isInViewport(sectionElement) === "yes") {
+      sectionElement.classList.add("active");
+    } else {
+      sectionElement.classList.remove("active");
+    }
+  });
+}
+
+function isInViewport(Element) {
+  const rect = Element.getBoundingClientRect();
+  if (rect.top <= 80 && rect.bottom >= 80) {
+    return "yes";
+  } else {
+    return "no";
+  }
+}
